7d22f4a5178170190ec2ee45285bf8aa1abfd599
mamun-prodhan <mamunpr9@gmail.com>
2022-12-27 17:01:52 +0600

    page structure completed

---
+++ src/Pages/CompletedTask/CompletedTask.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CompletedTask = () => {
+    return (
+        <div>
+            This is completed task page.
+        </div>
+    );
+};
+
+export default CompletedTask;
---
+++ src/Pages/MyTask/MyTask.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyTask = () => {
+    return (
+        <div>
+            This is My Task page.
+        </div>
+    );
+};
+
+export default MyTask;
